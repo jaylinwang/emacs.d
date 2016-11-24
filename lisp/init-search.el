687ce2d9000a43cce8f0ce7099c7d945fcf4d8e3
JaylinWang <jaylin0019@gmail.com>
2016-11-24 22:51:42 +0800

    加入项目中文件搜索

--- lisp/init-search.el
+++ lisp/init-search.el
@@ -1,6 +1,10 @@
+;;------------------------------------------
 ;; 搜索相关
+;;------------------------------------------
 
-(require-package ' find-file-in-project)
-
+;; 项目中搜索文件
+(require-package 'find-file-in-project)
+(global-set-key "\M-p" 'find-file-in-project)
+ 
 (provide 'init-search)
 
