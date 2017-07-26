91892356153122b1de2079917ebd7e41aff17b98
jaylinwang <jaylin0019@gmail.com>
2017-07-26 10:52:26 +0800

    去除linenumber

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
 (require 'init-fonts)
 
 (require 'init-project)
-(require 'init-linum)
+;;(require 'init-linum) //代码行数
 (require 'init-custom)
 (require 'init-themes)
 (require 'init-editing-utils)
@@ -40,3 +40,17 @@
 (require 'init-keymap-custom)
 
 (load-theme 'solarized-light t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode scss-mode vue-mode web-mode js2-mode markdown-mode lua-mode web-beautify flycheck magit git-timemachine git-messenger gitconfig-mode gitignore-mode git-blamed emmet-mode dumb-jump window-numbering spacemacs-theme solarized-theme projectile default-text-scale company smex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
